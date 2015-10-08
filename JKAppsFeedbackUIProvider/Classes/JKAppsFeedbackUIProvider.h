48bfac12a7614ee9721b41854d7f1c6951138085
Jayesh Kawli <j.kawli@gmail.com>
2015-10-07 21:27:43 -0400

    Added app specific header to combine all related files

---
+++ JKAppsFeedbackUIProvider/Classes/JKAppsFeedbackUIProvider.h
@@ -0,0 +1,16 @@
+//
+//  JKAppsFeedbackUIProvider.h
+//  JKAppsFeedbackUIProvider
+//
+//  Created by Jayesh Kawli Backup on 10/7/15.
+//  Copyright © 2015 Jayesh Kawli Backup. All rights reserved.
+//
+
+#ifndef JKAppsFeedbackUIProvider_h
+#define JKAppsFeedbackUIProvider_h
+
+#endif
+
+#import <EDColor/EDColor.h>
+#import "JKAppsFeedbackProviderViewController.h"
+#import "JKFeedbackInfo.h"
